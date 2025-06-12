7752b75c0e0e504090a5e797a43d1bfd6e104f21
JayaprabhuD-cloud <mail2jayaprabhu@gmail.com>
2025-06-12 11:19:56 +0000

    Checking Infra

--- main.tf
+++ main.tf
@@ -38,22 +38,7 @@ module "alb" {
 
 module "ecs" {
   source                    = "./modules/ecs"
-  cluster_name              = var.cluster_name
+  eks_cluster_name          = var.eks_cluster_name
+  public_subnets            = [module.vpc.public_subnet_1, module.vpc.public_subnet_2]
   private_subnets           = [module.vpc.private_subnet_1, module.vpc.private_subnet_2]
-  app_sg_name               = [module.security_groups.app_sg]
-  task_execution_role_arn   = module.iam.ecs_task_execution_role_arn
-  task_role_arn             = module.iam.ecs_task_role_arn
-  ecr_patient_repo_url      = module.ecr.patient_repo_url
-  ecr_appointment_repo_url  = module.ecr.appointment_repo_url
-  target_group_arns         = module.alb.target_group_arns
-  container_cpu             = var.container_cpu
-  container_memory          = var.container_memory
-  app_port                  = var.app_port
-  desired_capacity          = var.desired_capacity
-  appoinment_log_group_name = var.appoinment_log_group_name
-  appointment_service_task  = var.appointment_service_task
-  patient_service_name      = var.patient_service_name
-  appointment_service_name  = var.appointment_service_name
-  patient_service_task_name = var.patient_service_task_name
-  patient_log_group_name    = var.patient_log_group_name
 }
--- variables.tf
+++ variables.tf
@@ -27,11 +27,6 @@ variable "repo_name_1" {
   type = string
 }
 
-variable "repo_name_2" {
-  description = "Variable created for appoinment ecr repo"
-  type = string
-}
-
 variable "alb_sg_name" {
   description = "sg for alb"
   type = string
@@ -42,96 +37,7 @@ variable "app_sg_name" {
   type = string
 }
 
-variable "tg_name_patient" {
-  description = "Name of the target group for patient"
-  type        = string
-}
-
-variable "tg_name_appoinment" {
-  description = "Name of the target group for appointment"
-  type        = string
-}
-
 variable "alb_name" {
   description = "Name of the Application Load Balancer"
   type        = string
-}
-
-variable "ecs-task-execution-role-name" {
-  type = string
-}
-
-variable "ecs_task_execution_role_policy_name" {
-  type = string
-}
-variable "ecs_task_role" {
-  type = string
-}
-
-variable "ecs_task_role_policy" {
-  type = string
-}
-
-variable "container_cpu" {
-  description = "CPU units for containers"
-  type        = number
-  default     = 256
-}
-
-variable "container_memory" {
-  description = "Memory for containers"
-  type        = number
-  default     = 512
-}
-
-variable "app_port" {
-  description = "Application port"
-  type        = number
-  default     = 3000
-}
-
-variable "desired_capacity" {
-  description = "Desired number of instances"
-  type        = number
-  default     = 1
-}
-
-variable "cluster_name" {
-  type = string
-  default = "usecase9_cluster"
-}
-
-variable "appoinment_log_group_name" {
-  type = string
-  default = "appoinment_log_group"
-}
-
-variable "appointment_service_task" {
-  type = string
-  default = "appointment_service_task_definition"
-}
-
-variable "patient_service_name" {
-  type = string
-  default = "patient_service_name"
-}
-
-#variable "appointment_service_task" {
-#  type = string
-#  default = "appointment_service_name"
-#}
-
-variable "patient_service_task_name" {
-  type = string
-  default = "patient_service_task_definition"
-}
-
-variable "patient_log_group_name" {
-  type = string
-  default = "patient_log_group"
-}
-
-variable "appointment_service_name" {
-  type = string
-  default = "appointment_service_name"
 }
